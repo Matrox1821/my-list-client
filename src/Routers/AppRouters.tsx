b9251d965d0da5b59cf73eb82ba67595bb8a78d9
Matrox1821 <matiasgg1821@gmail.com>
2022-04-19 01:28:07 -0300

    first commit

---
+++ src/Routers/AppRouters.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { routes } from "../helpers/routes";
+import { Layout } from "../Layout";
+import { HomePage } from "../Pages/HomePage";
+export const AppRouters: FC = () => {
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path={routes.home} element={<HomePage />} />
+                </Routes>
+                {/* <Route path="*" component={NotFoundPage} /> */}
+            </Layout>
+        </Router>
+    );
+};
+/* import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout } from "../layout";
+import { routes } from "./helpers/routes";
+export const AppRouters = () => {
+    return (
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path={routes.home} component={HomePage} />
+
+                    <Route path="*" component={NotFoundPage} />
+                </Switch>
+            </Layout>
+        </Router>
+    );
+};
+ */
