5db009a93c36db7c329c354f34be8eafce23aa04
Matrox1821 <matiasgg1821@gmail.com>
2022-04-21 17:30:46 -0300

    changes in styles

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,23 +1,28 @@
 import React, { FC, useState, useEffect, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../img/Logo.svg";
-import { StyledHeader, StyledLogo, StyledNav } from "./HeaderStyles";
-import { MenuIconStyled } from "./MenuIcon/MenuIcon";
+import "./Header.css";
 const LogoComponent = () => {
     return (
-        <StyledLogo>
+        <div className="logo">
             <Link to="/">
                 <img src={Logo} alt="logo" />
             </Link>
             {/* <Link to="/">
                 <span>MyList</span>
             </Link> */}
-        </StyledLogo>
+        </div>
     );
 };
 const NavComponent = () => {
+    const Items = [
+        { name: "Home", rute: "/" },
+        { name: "My List", rute: "/my-list" },
+        { name: "Catalog", rute: "catalog" },
+    ];
     const [openMenu, setOpenMenu] = useState<boolean>(false);
     const [width, setWidth] = useState<number>(window.innerWidth);
+    const MobileSize = width <= 960;
     const MenuState = () => {
         setOpenMenu(!openMenu);
     };
@@ -33,30 +38,54 @@ const NavComponent = () => {
         };
     });
     return (
-        <StyledNav size={width} menu={openMenu}>
-            <ul>
-                <li className="option">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="option">
-                    <Link to="/my-list">My List</Link>
-                </li>
-                <li className="option">
-                    <Link to="/catalog">Catalog</Link>
-                </li>
-            </ul>
-            <div onClick={MenuState}>
-                <MenuIconStyled />
-            </div>
-        </StyledNav>
+        <nav>
+            {MobileSize ? (
+                <Fragment>
+                    <button
+                        className={openMenu ? "menu-icon active" : "menu-icon"}
+                        type="button"
+                        onClick={MenuState}
+                    >
+                        <span className="line" />
+                        <span className="line" />
+                        <span className="line" />
+                    </button>
+                    <ul
+                        className={
+                            openMenu ? "menu-moblie active" : "menu-moblie"
+                        }
+                    >
+                        {Items.map((item) => {
+                            return (
+                                <li className="item mobile">
+                                    <Link className="link" to={item.rute}>
+                                        <span>{item.name}</span>
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </Fragment>
+            ) : (
+                <ul className="menu-desktop">
+                    {Items.map((item) => {
+                        return (
+                            <li className="item desktop">
+                                <Link to={item.rute}>{item.name}</Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </nav>
     );
 };
 
 export const Header: FC = () => {
     return (
-        <StyledHeader>
+        <header>
             <LogoComponent />
             <NavComponent />
-        </StyledHeader>
+        </header>
     );
 };
