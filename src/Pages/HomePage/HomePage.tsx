91db83f824f450298acdfd3e290d0175886b387b
valevilner <vaale-3@hotmail.com>
2022-06-01 18:15:14 -0300

    test

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
         <h1>My List</h1>
         <p>
           This is a page of lists, where you can search and create list of
-          things that call your attention.
+          things that call your attention..
         </p>
       </div>
     </section>
